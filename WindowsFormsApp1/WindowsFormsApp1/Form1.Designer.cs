cc879ef921070354cab30206efc4637ff3ce0429
Barış Savak <128433645+bariss30@users.noreply.github.com>
2024-03-18 11:53:39 -0400

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TAŞ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(557, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 67);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "TAŞ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(153, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 69);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "KAĞIT";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(557, 104);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 69);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "KAĞIT";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(153, 187);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 64);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "MAKAS";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(557, 183);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(97, 68);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "MAKAS";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(238, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 108);
+            this.label1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(352, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(352, 117);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(109, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(352, 201);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(109, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 359);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int secilenSayi = rnd.Next(0, 3);
+            button1.BackColor = SystemColors.Control;
+            button3.BackColor = SystemColors.Control;
+            button5.BackColor = SystemColors.Control;
+            button2.BackColor = SystemColors.Control;
+            button4.BackColor = SystemColors.Control;
+            button6.BackColor = SystemColors.Control;
+
+
+            switch (secilenSayi)
+            {
+                case 0:
+                    button1.BackColor = Color.Blue;
+                    button2.BackColor=Color.Blue;
+                    label1.Text = "beraber";
+                   
+                  
+                    break;
+                case 1:
+                    button2.BackColor = Color.Red;
+                    button3.BackColor = Color.Green;
+                    label1.Text = "Kaybettin";
+                    break;
+                case 2:
+                    button2.BackColor = Color.Green;
+                    button5.BackColor = Color.Red;
+                    label1.Text = "Kazzandın";
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz sayı.");
+                    break;
+            }
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int secilenSayi = rnd.Next(0, 3);
+            button1.BackColor = SystemColors.Control;
+            button3.BackColor = SystemColors.Control;
+            button5.BackColor = SystemColors.Control;
+            button2.BackColor = SystemColors.Control;
+            button4.BackColor = SystemColors.Control;
+            button6.BackColor = SystemColors.Control;
+
+
+            switch (secilenSayi)
+            {
+                case 0:
+                    button4.BackColor = Color.Green;
+                    button1.BackColor = Color.Red;
+                    label1.Text = "Kazandın";
+
+
+                    break;
+                case 1:
+                    button4.BackColor = Color.Blue;
+                    button3.BackColor = Color.Blue;
+                    label1.Text = "Berabere";
+                    break;
+                case 2:
+                    button4.BackColor = Color.Red;
+                    button5.BackColor = Color.Green;
+                    label1.Text = "Kaybettin";
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz sayı.");
+                    break;
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int secilenSayi = rnd.Next(0, 3);
+            button1.BackColor = SystemColors.Control;
+            button3.BackColor = SystemColors.Control;
+            button5.BackColor = SystemColors.Control;
+            button2.BackColor = SystemColors.Control;
+            button4.BackColor = SystemColors.Control;
+            button6.BackColor = SystemColors.Control;
+
+
+            switch (secilenSayi)
+            {
+                case 0:
+                    button6.BackColor = Color.Red;
+                    button1.BackColor = Color.Green;
+                    label1.Text = "Kaybettin";
+
+
+                    break;
+                case 1:
+                    button6.BackColor = Color.Green;
+                    button3.BackColor = Color.Red;
+                    label1.Text = "Kazandın";
+                    break;
+                case 2:
+                    button6.BackColor = Color.Blue;
+                    button5.BackColor = Color.Blue;
+                    label1.Text = "Berabere";
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz sayı.");
+                    break;
+            }
+
+        }
+    
+    
+    }
+}
